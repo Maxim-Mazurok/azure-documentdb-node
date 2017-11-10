eff4d304eec232bac978d5aa184dcf20459264e1
Maxim Mazurok <maxim@mazurok.com>
2017-11-10 04:03:08 +0200

    i love node

--- samples/Shared/config.js
+++
@@ -1,9 +0,0 @@
-﻿exports.connection = {  
-    endpoint: 'YOUR_ENDPOINT_GOES_HERE',  
-    authKey: 'YOUR_AUTH_KEY_GOES_HERE'  
- };  
- 
- exports.names = {  
-     database: 'NodeSamples',  
-     collection: 'Data',  
- };  
